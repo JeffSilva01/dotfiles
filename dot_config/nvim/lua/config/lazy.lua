9bfde2b2f02a4dec2720883ecd8a1959baec4c3d
Jefferson Silva <jeffsilva01.dev@gmail.com>
2025-02-08 14:04:17 -0300

    Fix typo in LazyVim Python plugin import
    Corrected the import statement for the Python language plugin in the LazyVim configuration. Changed "pyton" to "python" to ensure proper loading of Python-related features and tools.

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.angular" },
     { import = "lazyvim.plugins.extras.lang.prisma" },
-    { import = "lazyvim.plugins.extras.lang.pyton" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.docker" },
