df0a0ddc0d4f4406f76f69b08184fbaa7d268b85
Jefferson Silva <jeffsilva01.dev@gmail.com>
2026-04-04 12:28:08 -0300

    update

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -1,13 +1,27 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = {
-    style = "moon",
-    transparent = true,
-    terminal_colors = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "moon",
+      transparent = true,
+      terminal_colors = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim", opts = {
+    transparent_mode = true,
+  } },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
     },
   },
 }
